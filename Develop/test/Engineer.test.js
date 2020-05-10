dfb92a26d089fcbe4851ebd350c17d5c36c50430
Phillip Manapat <philmanapat@gmail.com>
2020-05-09 22:57:13 -0400

    Finished

--- Develop/test/Engineer.test.js
+++ Develop/test/Engineer.test.js
@@ -3,6 +3,7 @@ const Engineer = require("../lib/Engineer");
 test("Can set GitHUb account via constructor", () => {
   const testValue = "GitHubUser";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
+  console.log(e);
   expect(e.github).toBe(testValue);
 });
 
